b57576d3fb29996135b0696b23ddbb065ca66746
ataa599 <attasiddiqui101@gmail.com>
2025-09-24 21:57:24 +0100

    predict pipeline tested and move on to deployment

--- testing.py
+++ testing.py
@@ -5,6 +5,7 @@ from src.logger import logging
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
+from src.pipline.predict_pipeline import PredictPipeline, CustomData
 
 if __name__ == "__main__":
     # try:
@@ -26,6 +27,20 @@ if __name__ == "__main__":
     obj_model_trainer = ModelTrainer()
     r2_score = obj_model_trainer.initiate_model_trainer(train_array, test_array)
     print(f"The accuracy of the best model is {r2_score}")
+
+    # Checking predict pipline
+
+    obj_predict_dataframe = CustomData("female", "group C", "bachelor's degree", "standard", "none", 54, 76)
+
+    pred_df = obj_predict_dataframe.get_data_as_data_frame()
+
+    obj_predict_pipeline = PredictPipeline()
+
+    results = obj_predict_pipeline.predict(pred_df)
+
+    print(f"the results of the prediction is {results}")
+
+
 
     
 
